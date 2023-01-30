c2eef0d5a9ba19ca63ec05b0068ec942046290dc
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2023-01-30 23:04:46 +0200

    add

--- contacts.js
+++ contacts.js
@@ -1,1 +1,12 @@
-const contactsPath = require("./db/contacts.json");
+const invokeAction = async ({ action, id, data }) => {
+  switch (action) {
+    case "listContacts":
+      const contacts = await contactsOperations.listContacts();
+      console.log(contacts);
+      break;
+    case "getContactById":
+      const contact = await contactsOperations.getContactById(id);
+      console.log(contact);
+      break;
+  }
+};
